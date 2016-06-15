00ff88afff068bdd09fac1bddd4fcc99cd00d201
Marina Flohr <marina@imperia.bg>
2016-06-15 14:29:20 +0300

    Custom scss added.
    Default font changed.
    
    Custom scss added.
    
    Default font changed.
    
    Custom colors.
    
    Custom CSS - flat and raised button styles
    
    Default angular material theme extended with custom colors.
    
    Redundant styles removed.

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -101,16 +101,17 @@ app.config(['$routeProvider', '$locationProvider', '$httpProvider', '$mdThemingP
   $mdThemingProvider.theme('default')
 
     .primaryPalette('delosBlue', {
-      'default': '700', 
+      'default': '700', // by default use shade 400 from the pink palette for primary intentions
       // 'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
       // 'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
       // 'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
     })
     .accentPalette('delosGreen', {
-      'default': '700' // use shade 700 for default, and keep all other shades the same
+
+      'default': '700' // use shade 200 for default, and keep all other shades the same
     })
     .warnPalette('delosRed', {
-      'default': '700' // use shade 700 for default, and keep all other shades the same
+      'default': '700' // use shade 200 for default, and keep all other shades the same
     });
 
 
